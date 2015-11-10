c52f79192be37eef4e4ce333a3525487efb023be
Mark McCaskey <mmccask2@gmu.edu>
2015-11-09 19:34:45 -0500

    Fix warnings

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::env;
-use std::io;
+//use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 /*use sdl2::keyboard::Keycode;
 use sdl2::keyboard::Scancode;*/
-use std::collections::HashSet;
-use std::ops;
-use std::path::Path;
+//use std::collections::HashSet;
 
 mod state;
 
@@ -39,21 +37,22 @@ fn main() {
     let mut state: state::State = Default::default(); 
     let args: Vec<_> = env::args().collect();
 
-    if( args.len() < 2 )
+    if args.len() < 2 
     {
-        panic!("Enter the name of a file to run");
+        print!("Enter the name of a file to run");
     }
     
     let arg: &String = &args[1];
-    let mut fin = File::open(arg)
+    let fin = File::open(arg)
         .ok()
         .expect("File not found");
 
-//    let in_vals = fin.read_i8().unwrap();
+//    let in_vals = fin.read_u8().unwrap();
 
     let mut i = 0x200;
     for x in fin.bytes() {
-        state.memory[i] = x.unwrap() as i8;
+        state.memory[i] = x.unwrap();
+        i+=1;
     }
 
     //start loop here
--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,13 @@
 use std::mem;
 
 pub struct State {
-    pub memory:         [i8; 4096],
-    registers:      [i8; 16],
-    stack:          [i16;16],
-    delay_timer:     i8,
-    sound_timer:     i8,
-    stack_pointer:   i16,
-    program_counter: i16,
+    pub memory:     [u8; 4096],
+    registers:      [u8; 16],
+    stack:          [u16;16],
+    delay_timer:     u8,
+    sound_timer:     u8,
+    stack_pointer:   u16,
+    program_counter: u16,
 }
 
 impl Default for State {
@@ -26,15 +26,15 @@ impl Default for State {
 }
 
 impl State {
-    fn dispatch(&mut self, opcode: i16) {
-        let b = ((opcode & 0x0F00) >> 8) as i8;
-        let c = ((opcode & 0x00F0) >> 4) as i8; 
+    fn dispatch(&mut self, opcode: u16) {
+        let b = ((opcode & 0x0F00) >> 8) as u8;
+        let c = ((opcode & 0x00F0) >> 4) as u8; 
         let x = b as usize;
         let y = c as usize;
-        let bcd = (opcode & 0xFFF) as i16;
-        let cd  = (opcode & 0xFF) as i8;
+        let bcd = (opcode & 0xFFF) as u16;
+        let cd  = (opcode & 0xFF) as u8;
         
-        match( opcode & 0xF000 >> 12 ) {
+        match (opcode & 0xF000) >> 12  {
             0 => match opcode {
                 0x00E0 => self.clear_screen(),
                 0x00EE => self.return_op(),
@@ -50,7 +50,7 @@ impl State {
             8  => self.arithmetic_dispatch( opcode ),
             9  => self.skip_if_xneqy( x, y),
             10 => self.program_counter = bcd,
-            11 => self.program_counter = bcd + (self.registers[0] as i16),
+            11 => self.program_counter = bcd + (self.registers[0] as u16),
             12 => panic!("Opcode 0xCXXX not implemented!"),//random number stuff later
             13 => panic!("Opcode 0xDXXX not implemented!"),
             14 => panic!("Opcode 0xEXXX not implemented!"),
@@ -59,16 +59,16 @@ impl State {
         }
     }
 
-    fn f_dispatcher(&mut self, opcode: i16) {
+    fn f_dispatcher(&mut self, opcode: u16) {
         let b = (opcode & 0x0F00) >> 8;
         let x = b as usize;
 
-        match( opcode & 0xFF ) {
+        match opcode & 0xFF {
             0x07 => self.registers[x] = self.delay_timer,
             0x0A => panic!("Opcode FXXA not implemented!"),
             0x15 => self.delay_timer = self.registers[x],
             0x18 => self.sound_timer = self.registers[x],
-            0x1E => self.program_counter += (self.registers[x] as i16),
+            0x1E => self.program_counter += self.registers[x] as u16,
             0x29 => panic!("Opcode FX29 not implemented!"),
             0x33 => panic!("Opcode FX33 not implemented!"),
             0x55 => panic!("Opcode FX55 not implemented!"),
@@ -84,43 +84,44 @@ impl State {
     pub fn run_opcode(&mut self) {
         self.increment_pc();
         let first_byte = self.memory[
-            (self.program_counter-2) as usize] as i16; 
+            (self.program_counter-2) as usize] as u16; 
         let second_byte = self.memory[
-            (self.program_counter-1) as usize] as i16;
+            (self.program_counter-1) as usize] as u16;
         self.dispatch( second_byte | (first_byte << 8) );
     }
 
-    fn arithmetic_dispatch(&mut self, opcode: i16 ) {
+    fn arithmetic_dispatch(&mut self, opcode: u16 ) {
         let b = (opcode & 0xF00) >> 8; //0x0x00
         let c = (opcode & 0xF0)  >> 4;  //0x00y0 
         let x = b as usize;
         let y = c as usize;
-        match (opcode & 0xF) {
+        match opcode & 0xF {
             0x0 => self.registers[x]  = self.registers[y], 
             0x1 => self.registers[x] |= self.registers[y],
             0x2 => self.registers[x] &= self.registers[y],
             0x3 => self.registers[x] ^= self.registers[y],
             0x4 => self.arithmetic_four(x, y),
             0x5 => self.arithmetic_five(x, y),
-	    0x6 => self.arithmetic_six(x, y),
+	    0x6 => self.arithmetic_six(x),
 	    0x7 => self.arithmetic_seven(x, y),
-            0xE => self.arithmetic_fourteen(x, y),               _   => panic!("Opcode {} not recognized", opcode),
+            0xE => self.arithmetic_fourteen(x),
+            _   => panic!("Opcode {} not recognized", opcode),
         }
     }
 
     fn arithmetic_four(&mut self, x: usize, y: usize) {
 	// Stores Vy + Vx into Vx and sets VF = carry      
-        let xl = self.registers[x] as i16;
-        let yl = self.registers[y] as i16;
+        let xl = self.registers[x] as u16;
+        let yl = self.registers[y] as u16;
         self.registers[x] += self.registers[y];
-        if((xl+yl>>8)>1){
+        if (xl+yl>>8) > 1 {
             self.registers[0xF]=1
         }
     }
 
     
     fn arithmetic_five(&mut self, x: usize, y: usize) {
-        if(self.registers[x] > self.registers[y]) {
+        if self.registers[x] > self.registers[y] {
             self.registers[0xF]=1;
         }
         self.registers[x] -= self.registers[y];
@@ -128,45 +129,45 @@ impl State {
 
 
     //Sets VF as the least sigificant bit of Vx Then Vx is divided by 2  
-    fn arithmetic_six(&mut self, x: usize, y: usize) {
+    fn arithmetic_six(&mut self, x: usize) {
         self.registers[0xF] = self.registers[x] & 0x1;
         self.registers[x] >> 1;
     }
 
 
     fn arithmetic_seven(&mut self, x: usize, y: usize) {
-        if(self.registers[x] < self.registers[y]) {
+        if self.registers[x] < self.registers[y] {
             self.registers[0xF] = 1;
         }
         self.registers[x] = self.registers[y]
             - self.registers[x];
     }
 
-    fn arithmetic_fourteen(&mut self, x: usize, y: usize) {
+    fn arithmetic_fourteen(&mut self, x: usize) {
         self.registers[0xF] = (self.registers[x] >> 7) & 1;
 	self.registers[x] << 1; 
     }
 
     fn skip_if_xeqy( &mut self, x: usize, y: usize ) {
-        if (self.registers[x] == self.registers[y]) {
+        if self.registers[x] == self.registers[y] {
             self.increment_pc();
         }
     }
 
     fn skip_if_xneqy( &mut self, x: usize, y: usize ) {
-        if (self.registers[x] != self.registers[y]) {
+        if self.registers[x] != self.registers[y] {
             self.increment_pc();
         }
     }
 
-    fn skip_if_eq( &mut self, x: usize, n: i8 ) {
-        if (self.registers[x] == n) {
+    fn skip_if_eq( &mut self, x: usize, n: u8 ) {
+        if self.registers[x] == n {
             self.increment_pc();
         }
     }
 
-    fn skip_if_neq( &mut self, x: usize, n: i8 ) {
-        if (self.registers[x] != n) {
+    fn skip_if_neq( &mut self, x: usize, n: u8 ) {
+        if self.registers[x] != n {
             self.increment_pc();
         }
     }
@@ -175,7 +176,7 @@ impl State {
         self.program_counter += 2;
     }
 
-    fn call_op( &mut self, address: i16 ) {
+    fn call_op( &mut self, address: u16 ) {
         self.stack[self.stack_pointer as usize] =
             self.program_counter;
 
